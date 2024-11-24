49ce7fc8134f5a2b5922852f4c481272aa368c90
dadreu <dadreu@gmail.com>
2024-11-24 22:52:55 +0500

    Создание модели данных. Драчёв

--- Weather_drachev_galkin/MainWindow.xaml.cs
+++ Weather_drachev_galkin/MainWindow.xaml.cs
@@ -9,6 +9,24 @@ namespace Weather_drachev_galkin
 {
     public partial class MainWindow : Window
     {
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void UpdateWeather_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    public class WeatherData
+    {
+        public string DateTime { get; set; }
+        public string Temperature { get; set; }
+        public string Pressure { get; set; }
+        public string Humidity { get; set; }
+        public string WindSpeed { get; set; }
+        public string FeelsLike { get; set; }
+        public string WeatherDescription { get; set; }
     }
 }
