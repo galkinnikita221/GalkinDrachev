ea7bd9ee651514380ac91a0e3bfb50ccd9b17f68
Nikita <galkinnikita228@gmail.com>
2024-11-24 21:57:33 +0500

    first commit

---
+++ Weather_drachev_galkin/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Weather_drachev_galkin
+{
+    public partial class MainWindow : Window
+    {
+
+    }
+}
---
+++ Weather_drachev_galkin/WeatherCahce.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using static Weather_drachev_galkin.MainWindow;
+
+namespace Weather_drachev_galkin
+{
+    public static class WeatherCache
+    {
+        
+    }
+
+}
